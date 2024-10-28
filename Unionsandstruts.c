a23015af88bc67edb640644fcc09663cf672814d
TAMcDougall <tm4615k@gre.ac.uk>
2024-10-28 15:01:04 +0000

    Add files via upload
    Section 9, unions and struts

---
+++ Unionsandstruts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+union TypeConverter {
+        int intValue;
+        float  floatValue;
+};
+
+int main() {
+        union TypeConverter converter;
+        //converter.floatValue = 3.14;
+        converter.intValue = 3;
+        printf("Union memory address: %p\n",&converter);
+        printf("Union.float = %.2f, memory address: %p\n", converter.floatValue, &converter.floatValue);
+        printf("Union.int = %d memory address: %p\n\n", converter.intValue,&converter.intValue);
+}
